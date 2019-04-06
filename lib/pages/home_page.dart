ae5f38e86fbf07c5739f6712c27359e73fe22b79
chenmeng <1487768100@qq.com>
2019-04-06 15:46:12 +0800

    feature: homeGridview

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../service/service_method.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'dart:convert';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,6 +13,7 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text('百姓生活+'),
@@ -23,8 +25,14 @@ class _HomePageState extends State<HomePage> {
             var data =  json.decode(snapshot.data.toString());
             List<Map> swiperDataList = (data['data']['slides'] as List).cast();
 
+            List<Map> navigatorList = (data['data']['category'] as List).cast();
+            if(navigatorList.length>10){
+                navigatorList.removeRange(10, navigatorList.length);
+            }
+
             return Column(children: <Widget>[
-              SwiperDiy(swiperDataList:swiperDataList,)
+              SwiperDiy(swiperDataList:swiperDataList,),
+              TopNavigator(navigatorList: navigatorList,)
             ],);
           }else{
             return Center(child: Text('加载中'),);
@@ -42,8 +50,10 @@ class SwiperDiy extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.instance = ScreenUtil(width: 750,height: 1334)..init(context);
     return Container(
-      height: 333.0,
+      height: ScreenUtil().setHeight(333),
+      width: ScreenUtil().setWidth(750),
       child: Swiper(
         itemBuilder: (BuildContext context,int index){
           return Image.network("${swiperDataList[index]['image']}",fit: BoxFit.fill,);
@@ -51,6 +61,40 @@ class SwiperDiy extends StatelessWidget {
         itemCount: swiperDataList.length,
         pagination: new SwiperPagination(),
         autoplay: true,
+      ),
+    );
+  }
+}
+
+
+class TopNavigator extends StatelessWidget {
+  final List navigatorList;
+
+  TopNavigator({Key key, this.navigatorList}) : super(key: key);
+
+  Widget _gridViewItemUI(BuildContext context,item) {
+    return InkWell(
+      onTap: (){print('点击了导航');},
+      child: Column(
+        children: <Widget>[
+          Image.network(item['image'],width:ScreenUtil().setWidth(95)),
+          Text(item['mallCategoryName'])
+        ],
+      ),
+    );
+  }
+
+   @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: ScreenUtil().setHeight(320),
+      padding: EdgeInsets.all(3.0),
+      child: GridView.count(
+        crossAxisCount: 5,
+        padding: EdgeInsets.all(4.0),
+        children: navigatorList.map((item){
+          return _gridViewItemUI(context, item);
+        }).toList(),
       ),
     );
   }
